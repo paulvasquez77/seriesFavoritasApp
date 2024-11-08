d26b1be0e59e3e5190be18b70a7a385f6db03ca9
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2024-11-07 19:42:03 -0500

    first commit

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+      <h1>footer</h1>
+    </div>
+  );
+}
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.css';
+
+export default function NavBar() {
+  return (
+    <div className={styles.container}>
+      <h1>navbar</h1>
+    </div>
+  );
+}
